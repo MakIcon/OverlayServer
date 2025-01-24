f3dbde36c13d82951bf9a059c47b6c87421880b3
Neverоятный <130914704+MakIcon@users.noreply.github.com>
2025-01-24 16:50:16 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+	"os"
+)
 
-func main() {
-	
-	fmt.Println("fewfwefwefwefwe:")
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// Указываем путь к файлу in.html в папке site_c
+	tmpl, err := template.ParseFiles("site_c/in.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
 }
 
+func main() {
+	port := "20059" // Устанавливаем порт 20059
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
+
+	// Запускаем сервер
+	println("Сервер запущен на http://localhost:" + port)
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
+		os.Exit(1)
+	}
+}
